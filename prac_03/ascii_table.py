49e26c08a118308f5ecf4e35ec07dc73cae2a5e4
ishaqueahmed <ishaque.ahmed@my.jcu.edu.au>
2020-08-27 16:53:44 +1000

    Copy ascii_table.py, word_generator.py

---
+++ prac_03/ascii_table.py
@@ -0,0 +1,25 @@
+"""
+CP5632 Practical 3
+
+Convert from character to ASCII code and vice versa, and display ASCII table
+"""
+
+LOWER_LIMIT = 33
+UPPER_LIMIT = 127
+
+
+def main():
+    character = input("Enter a character: ")
+    print("The ASCII code for {} is {}".format(character, ord(character)))
+
+    ascii_code = int(input("Enter a number between {} and {}: ".format(LOWER_LIMIT, UPPER_LIMIT)))
+    while ascii_code < LOWER_LIMIT or ascii_code > UPPER_LIMIT:
+        print("Number out of range.")
+        ascii_code = int(input("Enter a number between {} and {}: ".format(LOWER_LIMIT, UPPER_LIMIT)))
+    print("The character for {} is {}".format(ascii_code, chr(ascii_code)))
+
+    for a in range(LOWER_LIMIT, UPPER_LIMIT + 1):
+        print("{:>3}  {}".format(a, chr(a)))
+
+
+main()
---
+++ prac_03/word_generator.py
@@ -0,0 +1,25 @@
+"""
+CP5632 Practical 3
+
+Program to generate random words based on word format provided by the user.
+"""
+
+import random
+
+VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+
+def main():
+    word_format = str(input("Enter the word format (c for consonants, v for vowels): ")).lower()
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+
+    print(word)
+
+
+main()
