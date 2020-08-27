2186dc7388e7d7f92d9b34b2685db47a5ae86a2e
ishaqueahmed <ishaque.ahmed@my.jcu.edu.au>
2020-08-27 15:25:54 +1000

    Refactor temperatures.py and broken_score.py to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -4,15 +4,33 @@ CP5632 Practical 1
 Debugged version of program to determine score status
 """
 
-score = float(input("Enter score: "))
+import random
 
-if score > 100:
-    print("Invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-elif score >= 0:
-    print("Bad")
-else:
-    print("Invalid score")
+
+def main():
+    score = float(input("Enter score: "))
+    status = determine_status(score)
+    print(status)
+
+    score = random.randint(0, 100)
+    print("\nRandom score: {}".format(score))
+    status = determine_status(score)
+    print(status)
+
+
+def determine_status(score):
+    """Determine the status of a score"""
+    if score > 100:
+        status = "Invalid score"
+    elif score >= 90:
+        status = "Excellent"
+    elif score >= 50:
+        status = "Passable"
+    elif score >= 0:
+        status = "Bad"
+    else:
+        status = "Invalid score"
+    return status
+
+
+main()
