8bec987b963f25aa39b9e69259be62a6ac36dcf6
ishaqueahmed <ishaque.ahmed@my.jcu.edu.au>
2020-09-03 12:10:38 +1000

    Update subject_reader.py, change get_data() to return data as list, add display_details() to print out subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,6 +1,7 @@
 """
-CP1404/CP5632 Practical
-Data file -> lists program
+CP5632 Practical 4
+
+Program to read the data from file subject_data.txt, format it, and display the results
 """
 
 FILENAME = "subject_data.txt"
@@ -8,12 +9,18 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_details(data)
+
+
+def display_details(data):
+    for values in data:
+        print("{} is taught by {:^12} and has {:>3} students".format(values[0], values[1], values[2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,8 +29,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append(parts)
         print("----------")
     input_file.close()
+    return data
 
 
 main()
