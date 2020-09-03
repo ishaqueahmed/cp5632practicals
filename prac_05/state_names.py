9c84b69234c687910e110e52a67bcd9d3e4b3445
ishaqueahmed <ishaque.ahmed@my.jcu.edu.au>
2020-09-03 13:54:18 +1000

    Add state_names.py

---
+++ prac_05/state_names.py
@@ -0,0 +1,24 @@
+"""
+CP5632 Practical 5
+
+State names in a dictionary (file has been reformatted)
+"""
+
+
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
+                "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory",
+                "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
+
+print()
+for state_code in CODE_TO_NAME:
+    print("{:<3} is {:<}".format(state_code, CODE_TO_NAME[state_code]))
